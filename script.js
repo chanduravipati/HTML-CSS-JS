4594d2db7e8ec5db14b35426fd344712502d6219
ChanduRavipati56 <138272995+chanduravipati@users.noreply.github.com>
2024-08-08 15:09:13 +0530

    Add files via upload
    javascript day3

---
+++ script.js
@@ -0,0 +1,114 @@
+/*//dec varible
+let b="50"
+var a=50
+console.log(a,b)
+console.log("Praveen Kumar Vaska")
+//const
+const d=100
+console.log(d)
+//data types
+console.log(typeof(b))
+console.log(typeof a )
+
+let r=true;
+console.log(typeof r )
+let h
+console.log(typeof h) 
+let g= null
+console.log(typeof g)
+//type corecion
+let i="23"*10;
+console.log(i)
+//opertaors
+/*Arthematic(+,-,*,/,%)
+  Logical(&&,||,!)
+  relational(<,>,<=,>=,==,!=,!==,===)
+  asssignment
+  ternary
+  Bitwise
+*/
+/*let n = 2 ** 3;
+console.log(n)
+let z=2<<4; //2*1=2*2=4*2=8*2=16*2=32
+console.log(z)
+ let x=10 && 20;
+console.log(x)
+x=10 || 20;
+console.log(x)
+let w=10//10
+let y=++w;//11
+let t=w++;//11
+let v=--w;//11
+let u=w--;//10
+let f=u--;//11
+console.log(w+y+t+v+u+f)
+//conditional statements
+/*
+if
+if-else
+if-else-if
+nested if
+switch case
+*/
+
+/*if(9===9)
+    console.log("Yeah! '9' equals to '9'😉")
+else if(8===8)
+    console.log("Both are not same😒")
+else{
+    console.log("ur in else block")
+}
+
+if(1){
+    if(0){
+     console.log(75);
+    }
+    else{
+        if(2) console.log("52");
+        else console.log("World")
+    }
+}
+for(i=0;i<5;i++)
+{
+    console.log("VASKA "+i)
+}
+console.log(i)
+//fun withoutpara & without return
+function demo(){
+    console.log("Hello Vaska 🙋‍♀️");
+}
+let j=demo()
+console.log(j)
+// fun with parameter and with return
+function ex(a,b)
+{
+    console.log("Hello",a+b)
+}
+let k=ex(10,40)
+console.log(k)*/
+
+let a=50;
+console.log(a);
+
+var b=3930;
+let f=b;
+console.log(f)
+
+let c=50;
+let d=100+c;
+console.log(d)
+
+function work(){
+    console.log("world")
+    return;
+}
+let x=work()
+console.log(x)
+
+function works(c,d){
+    console.log("world",c+d);
+    return;
+}
+let X=works(10,10);
+
+
